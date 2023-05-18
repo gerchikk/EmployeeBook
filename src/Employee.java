3553da6cecdcea7dd48eaf4c2f57029cdfbafa6d
gerchikk <geraloi@yandex.ru>
2023-05-18 11:41:15 +0500

    Добавил счетчик

--- src/Employee.java
+++ src/Employee.java
@@ -1,21 +1,23 @@
 public class Employee {
     private String fullName;
-    private int department;
+    private String department;
     private int salary;
     private int id;
+    private static int counter = 1;
 
-    public Employee (String fullName, int department, int salary, int id) {
+    public Employee (String fullName, String department, int salary, int id) {
         this.fullName = fullName;
         this.department = department;
         this.salary = salary;
-        this.id = id;
+        this.id = getCounter();
+        counter++;
     }
 
     public String getFullName() {
         return this.fullName;
     }
 
-    public int getDepartment() {
+    public String getDepartment() {
         return this.department;
     }
 
@@ -26,14 +28,25 @@ public class Employee {
     public int getId() {
         return this.id;
     }
+    public int getCounter() {
+        return counter;
+    }
 
-    public void setDepartment(int department) {
+    public void setDepartment(String department) {
         this.department = department;
     }
 
     public void setSalary (int salary) {
         this.salary = salary;
     }
+
+    public String toString () {
+        return "ФИО - " + this.fullName + "\n Отдел - " + this.department + "\n Зарплата - " + this.salary + "\n id - " + this.id;
+    }
+
+
+
+
 
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,25 @@
 public class Main {
+
+    public static  void separation () {
+        System.out.println("________________________________");
+    }
     public static void main(String[] args) {
-        Employee petrov = new Employee("Петров Иван Федорович", 1, 78960, 1);
-        Employee sidorov = new Employee();
-        Employee kompotov = new Employee();
-        Employee kapustin = new Employee();
-        Employee pirogov = new Employee();
+        Employee petrov = new Employee("Петров Иван Федорович", "1", 78960, 1);
+        Employee sidorov = new Employee("Сидоров Алексей Павлович", "2", 68920, 3);
+        Employee kompotov = new Employee("Компотов Петр Александрович", "3", 89650, 3);
+        Employee kapustin = new Employee("Капустин Сергей Фиолентович", "4", 74250, 3);
+        Employee pirogov = new Employee("Пирогов Аркадий Леонидович", "5", 83650, 5);
+        separation();
+        System.out.println(petrov);
+        separation();
+        System.out.println(sidorov);
+        separation();
+        System.out.println(kompotov);
+        separation();
+        System.out.println(kapustin);
+        separation();
+        System.out.println(pirogov);
+        separation();
 
     }
+}
